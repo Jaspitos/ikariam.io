a2d43fbdc257334cc4680bb0886ce8fc007d46cd
Jaspitos <javier17gz@gmail.com>
2016-09-10 15:16:59 +0200

    up

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 var cookieParser = require('cookie-parser');
+var session = require('express-session');
+var MongoStore = require('connect-mongo')(session);
 var dbprop = require('./server/properties/db-properties');
 
 if(app.get('env') == 'development')
@@ -10,7 +12,7 @@ if(app.get('env') == 'development')
 		else
 			process.env.NODE_ENV = 'production';
 
-	
+
 
 
 app.set('port', process.env.PORT || 3000);
@@ -28,14 +30,30 @@ app.engine('html', require('ejs').renderFile);
  app.use(bodyParser.urlencoded({ extended: true }));
  app.use(express.static(__dirname + '/public')); //Defines public folder files as static (css,js,img..)
 
+ 	dbprop = dbprop.loadDbProperties(process.env.NODE_ENV);
+ 	var dbHost = dbprop['app'].dbHost;
+ 	var dbPort = dbprop.dbPort;
+ 	var dbName = dbprop.dbName;
+
+ 	//TODO
+ 	var dbURL = 'mongodb://'+dbHost+':'+dbPort+'/'+dbName;
+ 	if (app.get('env') == 'production'){
+ 	dbURL = 'mongodb://devel:vivaeta@ds021036.mlab.com:21036/ikariam';
+ 	}
+
+ 	//TODO: Opens session
+ 	app.use(session({
+ 	secret: 'faeb4453e5d14fe6f6d04637f78077c76c73d1b4',
+ 	proxy: true,
+ 	resave: true,
+ 	saveUninitialized: true,
+ 	store: new MongoStore({ url: dbURL })
+ 	})
+ 	);
  //Module of routes conf
  require('./server/routes/routes')(app);
- 
+
 //Starts server
  var server = require('http').createServer(app).listen(app.get('port'), function(){
  console.log('Express server listening on port ' + app.get('port'));
  });
- 
- 
- 
- 
--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,35 +1,83 @@
 var logindao = require('../dao/logindao');
-var k = "";
 
 module.exports = function (app){
 
       / Http get request to signup page /
       app.get('/', function(req, res) {
-       // create a new user
-       res.render('login', { title: 'Entrar' });
+       //If session is live then redirect to index
+							if(req.session.user != null){
+								 res.render('inicio',{title:"Inicio"});
+							}
+
+							//Checks if the credentials are saved in clients cookies
+							else if(req.cookies.user == undefined || req.cookies.pass == undefined){
+								res.render('login', { title: 'Entrar'});
+								//console.log("Cookies Not available");
+							}
+							else{
+							// attempt automatic login //
+							logindao.autoLogin(req.cookies.user, req.cookies.pass, function(o){
+								if (o != null){
+									req.session.user = o;
+									 res.render('inicio', {title:"Inicio"});
+								}	else{
+									res.render('login', { title: 'Entrar' });
+								}
+							});
+						}
       });
-	  
-	  
+
+
 	  /* Http post request to submit login */
 	  app.post('/login', function(req, res){
 							logindao.manualLogin(req.body['userLogin'], req.body['passLogin'], function(e, o){
 								if (!o){
 									res.status(400).send(e);
 								}	else{
-									//req.session.user = o;
+									req.session.user = o;
+									if (req.body['remember-me'] == 'true'){
+										res.cookie('user', o.user, { maxAge: 900000 });
+										res.cookie('pass', o.pass, { maxAge: 900000 });
+									}
 									res.status(200).send(o);
 								}
 							});
 						});
-	  
+
 	  app.get('/signup', function(req, res) {
-		res.render('signup', { title: 'Registro' });
-		k = req.query.key;
+		    res.render('signup', {title: 'Registro'});
 	  });
-	  
+	  /* Http get request to check ur login state */
+						app.get('/',function(req,res){
+
+							//If session is live then redirect to index
+							if(req.session.user != null){
+								 res.render('inicio',{title:"Inicio"});
+							}
+
+							//Checks if the credentials are saved in clients cookies
+							else if(req.cookies.user == undefined || req.cookies.pass == undefined){
+								res.render('login', { title: 'Log In'});
+								//console.log("Cookies Not available");
+							}
+							else{
+							// attempt automatic login //
+							logindao.autoLogin(req.cookies.user, req.cookies.pass, function(o){
+								if (o != null){
+									req.session.user = o;
+									res.render('inicio',{title:"Inicio"});
+								}	else{
+									res.render('login', { title: 'Inicio' });
+								}
+							});
+						}
+
+
+					});
+
 	  / Http post request to signup new user /
       app.post('/signup', function(req, res) {
-		
+
 			logindao.checkUser(req.body['username'],function(er,ob)
 			{
 				if(ob == false)
@@ -51,21 +99,21 @@ module.exports = function (app){
 											res.status(200).send(o);
 										}
 									});
-							
+
 								} else res.status(400).send(error);
 							})
-							
+
 						} else res.status(400).send(err);
 					})
 				} else res.status(400).send(er);
 			})
-       
+
 
 		});
-		
+
 		app.get('/inicio', function(req, res) {
        // create a new user
        res.render('inicio', { title: 'Inicio' });
       });
-	  
+
 };
