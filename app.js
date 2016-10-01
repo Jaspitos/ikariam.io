17c4b8e61eb803bc68e4e3b68f473ebb0ebf2c7f
lorenzo <lorenzogamboagarcia@gmail.com>
2016-10-01 20:14:17 +0200

    front updates

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
-require('dotenv').config();
 /**
  *@Author: Javier y Lorenzo
  *@Desc: Starts up web app
  */
 
+ require('dotenv').config();
+
 //Importing modules
 var express = require('express'),
-    app     = express(),
-    http    = require('http').Server(app),
+    app = express(),
+    http = require('http').Server(app),
     bodyParser = require('body-parser'),
     session = require('express-session'),
     mongoose = require('mongoose'),
@@ -32,13 +33,11 @@ console.log(chalk.bold.green('Entorno elegido ----> ') + chalk.bold.yellow(proce
 
 mongoose.Promise = global.Promise;
 /*Check enviromemnt*/
-if(process.env.NODE_ENV == 'development')
-{
-  mongoose.connect(process.env.DBL_URI);
-  console.log(chalk.bold.bgGreen('mongo :: connected to database :: ikariam'));
-}
-else{
-  mongoose.connect(process.env.DBC_URI);
+if (process.env.NODE_ENV == 'development') {
+    mongoose.connect(process.env.DBL_URI);
+    console.log(chalk.bold.bgGreen('mongo :: connected to database :: ikariam'));
+} else {
+    mongoose.connect(process.env.DBC_URI);
 }
 
 
@@ -95,7 +94,7 @@ app.use(require('./server/routes/routes'));
 
 //Clients list connection
 var allClientsChat = [],
-  allClientsInicio = [];
+    allClientsInicio = [];
 
 
 //Starts general Chat
@@ -123,12 +122,10 @@ chat.on('connection', function(socket) {
     });
 
     socket.on('disconnect', function() {
-        if (!yaExiste)
-        {
-          allClientsChat.splice(allClientsChat.indexOf(user), 1);
-          chat.emit('disconnect', user, allClientsChat);
-        }
-        else chat.emit('disconnect', 'yaExiste', allClientsChat);
+        if (!yaExiste) {
+            allClientsChat.splice(allClientsChat.indexOf(user), 1);
+            chat.emit('disconnect', user, allClientsChat);
+        } else chat.emit('disconnect', 'yaExiste', allClientsChat);
 
 
 
@@ -154,12 +151,11 @@ inicio.on('connection', function(socket) {
 
 
     socket.on('disconnect', function() {
-        if (!yaExiste)
-        {
-          allClientsInicio.splice(allClientsInicio.indexOf(user), 1);
-          inicio.emit('disconnect', user, allClientsInicio);
+        if (!yaExiste) {
+            allClientsInicio.splice(allClientsInicio.indexOf(user), 1);
+            inicio.emit('disconnect', user, allClientsInicio);
         } else
-          inicio.emit('disconnect', 'yaExiste', allClientsInicio);
+            inicio.emit('disconnect', 'yaExiste', allClientsInicio);
 
 
     });
